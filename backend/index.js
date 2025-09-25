06501a99ab6329167e5ed38d736e64839706eba4
alexispelaye <alexispelaye@gmail.com>
2025-09-25 12:20:28 -0300

    feat(backend): add base auth

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import authRouter from './routes/auth.route';
 
 const port = 3000;
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hola mundo');
 })
+
+app.use('/api', authRouter);
 
 app.listen(port, () => {
   console.log(`Server listen on port ${port}`)
