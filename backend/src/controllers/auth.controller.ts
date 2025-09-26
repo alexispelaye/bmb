7fc0ab01d4f587753d4beeaf8051cef578261feb
alexispelaye <alexispelaye@gmail.com>
2025-09-25 23:18:15 -0300

    feat(backend): add db usage on auth controller

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,11 +1,19 @@
 import { Request, RequestHandler, Response } from "express";
 import { RequestWithUser, User } from "../middlewares/auth.middleware";
 import { signToken } from "../utils/jwt";
-import { comparePassword } from "../utils/password";
+import { comparePassword, hashPassword } from "../utils/password";
+import { pool } from "../config/db";
 
-export const login = (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response) => {
   const { user, password } = req.body;
-  if (!(user === 'admin' && comparePassword(password, 'admin'))) {
+  const dbResponse = await pool.query('SELECT * FROM users WHERE username = $1', [user]);
+  if (!dbResponse.rows.length) {
+    return res.status(401).json({
+      message: 'Invalid credentials'
+    })
+  }
+  const dbUser = dbResponse.rows[0];
+  if (!comparePassword(password, dbUser['password'])) {
     return res.status(401).json({
       message: 'Invalid credentials'
     });
@@ -19,6 +27,34 @@ export const me = (req: RequestWithUser, res: Response) => {
   return res.send(req.user);
 }
 
-export const register: RequestHandler = (req, res) => {
-  return res.send('Ups! This is not implemented yet');
+export const register: RequestHandler = async (req, res) => {
+  const { user, password, email } = req.body;
+  if (user === undefined) {
+    return res.status(400).json({
+      message: 'Username is required'
+    })
+  }
+  if (password === undefined) {
+    return res.status(400).json({
+      message: 'Password is required'
+    })
+  }
+  if (email === undefined) {
+    return res.status(400).json({
+      message: 'Email is required'
+    })
+  }
+  const dbResponse = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+  if (dbResponse.rows.length) {
+    return res.status(400).json({
+      message: 'User already exists 1'
+    })
+  }
+  const createUserResponse = await pool.query('INSERT INTO users (email, password, username) VALUES ($1, $2, $3)', [email, hashPassword(password), user]);
+  if (!createUserResponse.rowCount) {
+    return res.status(400).json({
+      message: 'User already exists 2'
+    })
+  }
+  return res.status(201).json({});
 }
