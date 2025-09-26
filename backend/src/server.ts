5aa35d1d6f6a4ddcb57a3ab406f5f698086625a5
alexispelaye <alexispelaye@gmail.com>
2025-09-25 22:04:53 -0300

    feat(backend): routes restructuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,13 @@
 import express from 'express';
-import authRouter from './routes/auth.route';
+import apiRouter from './routes/api.route';
 
 const port = 3000;
 const app = express();
 
 app.use(express.json());
-app.use('api')
 
-app.get('/', (req, res) => {
-  res.send('Hola mundo');
-})
-
-app.use('/auth', authRouter);
+app.use('api/', apiRouter);
 
 app.listen(port, () => {
   console.log(`Server listen on port ${port}`)
-})
+});
