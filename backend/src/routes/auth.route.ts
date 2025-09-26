3b5fa95b74d0c171a3722c27e237d433dfbf948f
alexispelaye <alexispelaye@gmail.com>
2025-09-25 23:19:26 -0300

    style(backend): just a slash

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import { register, login, me } from '../controllers/auth.controller';
+import { pool } from '../config/db';
 
 const router = Router();
-
+router.get('/test', async (req, res) => {
+  const rows = await pool.query('SELECT * FROM users');
+  console.log(rows);
+  return res.json(rows);
+})
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', me);
